1e4769e381e835f2f2d094519f245b5ebde55edf
yuen-kai <79285982+yuen-kai@users.noreply.github.com>
2021-10-08 21:29:46 -0400

    Repeating Tasks

--- AddTaskScreen.js
+++ AddTaskScreen.js
@@ -6,18 +6,18 @@ import RNPickerSelect from 'react-native-picker-select';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {Slider,Input} from 'react-native-elements'
+import {Slider,Input,CheckBox,Avatar} from 'react-native-elements'
 
-
-
-var tasks = []
+var savedTasks
 
 
 export default class AddTaskScreen extends React.Component {
-  editIndex
+  editName
   selectedTask
   edit = false
+  days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
   state = {
+    daysUsed:[false,false,false,false,false,false,false],
     date: new Date(),
     mode: 'date',
     show: false,
@@ -29,6 +29,9 @@ export default class AddTaskScreen extends React.Component {
     sortValue: 0,
     start: new Date(),
     end: new Date(),
+    weekly:false,
+    repeating: false,
+    // dueIncrease: 0,
   }
 
   componentDidMount(){
@@ -69,13 +72,12 @@ export default class AddTaskScreen extends React.Component {
   };
 
   getData = async () => {
+    let change = [...this.state.daysUsed]
+    change.splice(new Date().getDay(), 1,true)
+    this.setState({daysUsed:change})
       try {
         const jsonValue = await AsyncStorage.getItem('tasks')
-        tasks =  jsonValue != null ? JSON.parse(jsonValue) : null;
-        if(tasks == null){
-          tasks=[]
-        }
-        
+        savedTasks =  jsonValue != null ? JSON.parse(jsonValue) : null;
         this.editInfo()
         
       } catch(e) {
@@ -85,16 +87,28 @@ export default class AddTaskScreen extends React.Component {
       
    }
 
+
   editInfo = async () => {
     try {
-      const jsonValue = await AsyncStorage.getItem('editIndex')
       
-      this.editIndex =  jsonValue != null ? JSON.parse(jsonValue) : null;
-      if(this.editIndex != null){
-        this.selectedTask = tasks[this.editIndex]
+      const jsonValue = await AsyncStorage.getItem('editName')
+      this.editName = jsonValue != null ? JSON.parse(jsonValue) :null;
+      if(this.editName != null){
+        let change = [...this.state.daysUsed]
+        this.selectedTask = savedTasks[0][new Date().getDay()][savedTasks[0][new Date().getDay()].findIndex((task) =>task.name == this.editName)]
         this.edit = true
-        this.setState({date:new Date(new Date(this.selectedTask.date).getTime()),name:this.selectedTask.name,importance:this.selectedTask.importance,length:this.selectedTask.length,dueImportance:this.selectedTask.dueImportance})
-        await AsyncStorage.removeItem('editIndex')
+        this.setState({date:new Date(new Date(this.selectedTask.date).getTime()),name:this.selectedTask.name,importance:this.selectedTask.importance,length:this.selectedTask.length,dueImportance:this.selectedTask.dueImportance,repeating:this.selectedTask.repeating})
+        await AsyncStorage.removeItem('editName')
+          for(var i=0;i<=this.state.daysUsed.length-1;i++){
+            if(savedTasks[0][i].findIndex((task) =>task.name == this.editName)!=-1)
+            {
+              change.splice(i, 1,true)
+            }
+          };
+          if(savedTasks[1][new Date().getDay()].findIndex((task) =>task.name == this.editName)!=-1){
+            this.setState({weekly:true})
+          }
+          this.setState({daysUsed:change})
       }
     } catch(e) {
       Alert.alert('Failed to get edit info!','Failed to get edit info! Please try again.')
@@ -103,7 +117,7 @@ export default class AddTaskScreen extends React.Component {
     this.setState({ready:true})
  }
 
-  sortTask(){
+  sortTask(tasks){
     for(var i=tasks.length-1; i>=0; i--) {
       
       if(tasks[i].sortValue<=this.state.sortValue){
@@ -132,6 +146,12 @@ export default class AddTaskScreen extends React.Component {
     return hours+':'+minutes+' '+amPm
   }
 
+  changeDay(i){
+    let change = [...this.state.daysUsed]
+    change.splice(i, 1,!this.state.daysUsed[i])
+    this.setState({daysUsed:change})
+  }
+
   displayDate(date) {
     var month = new Date(date).getMonth()+1
     var day = new Date(date).getDate()
@@ -140,10 +160,22 @@ export default class AddTaskScreen extends React.Component {
 
   handleSave = async () => {
     var sameName = false
+    var count = 0
+    this.state.daysUsed.forEach(element => {
+      if(element){
+        count++
+      }
+    });
+    if(count>=2){
+      this.setState({repeating:true})
+    }
     await this.setState({sortValue: new Date(this.state.date).getTime()-((((this.state.importance*8)/100)*(this.state.length))/((6-this.state.dueImportance)*30))*24*60*60*1000})
-    this.selectedTask = {name:this.state.name, sortValue:this.state.sortValue, length: this.state.length, date: this.state.date, start:this.state.start, end:this.state.end, importance:this.state.importance,dueImportance:this.state.dueImportance}
+    var d = new Date().setHours(0,0,0,0)
+    var dueIncrease  = new Date(this.state.date).getTime()-new Date(d).getTime();
+    this.selectedTask = {name:this.state.name, sortValue:this.state.sortValue, length: this.state.length, date: this.state.date, start:this.state.start, end:this.state.end, importance:this.state.importance,dueImportance:this.state.dueImportance,repeating:this.state.repeating,dueIncrease:dueIncrease}
+
     if(this.edit == false){
-      tasks.forEach(element => {
+      savedTasks[0][new Date().getDay()].forEach(element => {
         if(element.name==this.selectedTask.name){
           sameName = true
         }
@@ -157,18 +189,24 @@ export default class AddTaskScreen extends React.Component {
       Alert.alert('Name Used','Name already used. Please select a new name.')
     }
     else{
-      
-      
-      //add new task to list of existing tasks
-      if(this.edit==true){
-        
-        tasks.splice(this.editIndex,1)
-      }
-      tasks.splice(this.sortTask(),0,this.selectedTask)
-      
+      for(var i=0;i<=this.state.daysUsed.length-1;i++){
+        if(this.state.daysUsed[i]==true){
+          if(this.edit==true){
+            savedTasks[0][i].splice(savedTasks[0][i].findIndex((task) =>task.name == this.editName),1)
+          }
+          savedTasks[0][i].splice(this.sortTask(savedTasks[0][i]),0,this.selectedTask)
+          if(this.state.weekly==true){
+            if(this.edit==true){
+              savedTasks[1][i].splice(savedTasks[1][i].findIndex((task) =>task.name == this.editName),1)
+            }
+            savedTasks[1][i].splice(this.sortTask(savedTasks[1][i]),0,this.selectedTask)
+          }
+        }
+      };
       //save data
       try {
-        const jsonValue = JSON.stringify(tasks)
+        
+        const jsonValue = JSON.stringify(savedTasks)
         await AsyncStorage.setItem('tasks', jsonValue)
       } catch (e) {
         console.log(e)
@@ -229,7 +267,7 @@ export default class AddTaskScreen extends React.Component {
               renderErrorMessage={false}
               keyboardType = "numeric"
               onChangeText={length => this.setState({length})}
-              value = {this.state.length!=0?(this.state.length).toString():null}
+              value = {this.state.length!=0?this.state.length.toString():null}
             />
           </View>
           <View style={styles.section}>
@@ -288,6 +326,30 @@ export default class AddTaskScreen extends React.Component {
             />
             </View>
           </View>
+          <Text style={{ fontSize: 17, padding:3 }}>Use for:</Text>
+        <CheckBox
+            title='Weekly'
+            checked={this.state.weekly}
+            onPress={() => this.setState({weekly: !this.state.weekly})}
+          />
+          <View style={{padding:8,flexDirection:'row',justifyContent: 'center',alignItems: 'center'}}>
+          
+          {
+            this.state.daysUsed.map((day, i) => {
+              return (
+                <Avatar key={i}
+                containerStyle={day==true?{backgroundColor:'#152075',margin:1}:{backgroundColor:'gray',margin:1}}
+                  size="small"
+                  rounded
+                  title={this.days[i].slice(0, 1)}
+                  onPress={() => this.changeDay(i)}
+                />
+              )
+            })
+          }
+          </View>
+          {this.state.daysUsed.includes(true)==false?<Text style={{ fontSize: 15, color: 'red', alignSelf:'center'}}>Nothing is selected!</Text>:null}
+      
           <TouchableOpacity
             onPress={() => this.handleSave()}
             style={[styles.button,{bottom:0,right:0,alignSelf:'flex-end',position:'absolute',paddingVertical:0}]}>
@@ -295,7 +357,8 @@ export default class AddTaskScreen extends React.Component {
           </TouchableOpacity>
           </View>
         </ScrollView>
-      </View>
+        </View>
+      
     );
   }
 }
